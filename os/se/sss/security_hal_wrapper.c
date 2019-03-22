384f000f89680a72797dd487c20cb973115e7e86
shbyeon <sh.byeon@samsung.com>
2019-03-22 17:38:10 +0900

    external/mbedtls: Remove artik dependency for DH in mbedtls
    - Replace artik-dependent isp function calls to security_hal function calls

--- os/se/sss/security_hal_wrapper.c
+++ os/se/sss/security_hal_wrapper.c
@@ -518,6 +518,7 @@ int hal_set_key(hal_key_type mode, uint32_t key_idx, hal_data *key, hal_data *pr
 			break;
 		case HAL_KEY_DH_1024:
 		case HAL_KEY_DH_2048:
+		case HAL_KEY_DH_4096:
 			key_type = SECURE_STORAGE_TYPE_KEY_DH;
 			break;
 		default:
@@ -608,6 +609,7 @@ static int hal_get_key_type(hal_key_type mode, unsigned int *key_type)
 			break;
 		case HAL_KEY_DH_1024:
 		case HAL_KEY_DH_2048:
+		case HAL_KEY_DH_4096:
 			*key_type = SECURE_STORAGE_TYPE_KEY_DH;
 			break;
 		default:
